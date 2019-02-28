95731aba2c4680df1cb9aeee299cdfee6c1d0e70
João Furtado <joao.furtado@outsystems.com>
2019-02-28 16:50:05 +0000

    main done. Still need to refactor the methods.

--- outsystems/pipeline/fetch_lifetime_data.py
+++ outsystems/pipeline/fetch_lifetime_data.py
@@ -1,6 +1,5 @@
 # Python Modules
-import os
-import sys
+import os, sys, argparse
 
 # Workaround for Jenkins:
 # Set the path to include the outsystems module
@@ -9,22 +8,68 @@ if "WORKSPACE" in os.environ:
   sys.path.append(os.environ['WORKSPACE'])
 
 # Custom Modules
-from outsystems.lifetime.lifetime_applications import get_applications
-from outsystems.lifetime.lifetime_environments import get_environments
+from outsystems.vars.file_vars import ARTIFACT_FOLDER
+from outsystems.vars.lifetime_vars import LIFETIME_HTTP_PROTO, LIFETIME_API_ENDPOINT, LIFETIME_API_VERSION
+
+#from outsystems.lifetime.lifetime_applications import get_applications
+#from outsystems.lifetime.lifetime_environments import get_environments
 
 ############################################################## VARS ##############################################################
 
 # Set script local variables from environment
 # LT url 
-lt_url = os.environ['LifeTimeEnvironmentURL']
+#lt_url = os.environ['LifeTimeEnvironmentURL']
 # LT Token for authentication
-lt_token = os.environ['AuthorizationToken']
+#lt_token = os.environ['AuthorizationToken']
 
 ############################################################## SCRIPT ##############################################################
+def main(artifact_dir : str, lt_http_proto :str, lt_url :str, lt_api_endpoint :str, lt_api_version :int, lt_token :str ):
+  # Get Environments
+#  get_environments(lt_url, lt_token)
+  print("OS Environments data retrieved successfully.")
+  # Get Applications without extra data
+#  get_applications(lt_url, lt_token, False)
+  print("OS Applications data retrieved successfully.")
 
-# Get Environments
-get_environments(lt_url, lt_token)
-print("OS Environments data retrieved successfully.")
-# Get Applications without extra data
-get_applications(lt_url, lt_token, False)
-print("OS Applications data retrieved successfully.")
+if __name__ == "__main__":
+  parser=argparse.ArgumentParser()
+  parser.add_argument("-a", "--artifacts", type=str, help="Name of the artifacts folder. Default: \"Artifacts\"")
+  parser.add_argument("-u", "--lt_url", type=str, help="URL for LifeTime environment, without the API endpoint. Example: \"https://<lifetime_host>\"")
+  parser.add_argument("-t", "--lt_token", type=str, help="Token for LifeTime API calls.")
+  parser.add_argument("-v", "--lt_api_version", type=int, help="LifeTime API version number. If version <= 10, use 1, if version >= 11, use 2. Example: 2")
+  parser.add_argument("-e", "--lt_endpoint", type=str, help="(optional) Used to set the API endpoint for LifeTime, without the version. Default: \"lifetimeapi/rest\"")
+
+  args = parser.parse_args()
+  # Parse the artifact directory
+  # Assumes the default dir = Artifacts
+  artifact_dir = ARTIFACT_FOLDER
+  if args.artifacts: artifact_dir = args.artifacts
+  # Parse the API endpoint
+  # Assumes the default endpoint = lifetimeapi/rest
+  lt_api_endpoint = LIFETIME_API_ENDPOINT
+  if args.lt_endpoint: lt_api_endpoint = args.lt_endpoint
+  # Parse the LT Url and split the LT hostname from the HTTP protocol
+  # Assumes the default HTTP protocol = https
+  lt_http_proto = LIFETIME_HTTP_PROTO
+  lt_url = args.lt_url
+  if lt_url:
+    if lt_url.startswith("http://"): 
+      lt_http_proto = "http"
+      lt_url = lt_url.replace("http://","")
+    else:
+      lt_url = lt_url.replace("https://","")
+    if lt_url.endswith("/"): lt_url = lt_url[:-1]
+  else:
+    print("You need to set the LifeTime URL.")
+    exit(1)
+  # Parte LT API Version
+  # Assumes the default version = 2
+  lt_version = LIFETIME_API_VERSION
+  if args.lt_api_version: lt_version = args.lt_api_version
+  # Parse the LT Token
+  lt_token = args.lt_token
+  if not lt_token: 
+    print("You need to set the LifeTime Token or else you won't be able to authenticate.")
+    exit(1)
+    
+  main(artifact_dir, lt_http_proto, lt_url, lt_api_endpoint, lt_version, lt_token)
