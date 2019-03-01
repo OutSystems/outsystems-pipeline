852f64e756f9ae3ad1a0f2ce36a08c7eab919ff3
João Furtado <joao.furtado@outsystems.com>
2019-03-01 15:16:52 +0000

    refactor cicd vars to remove the environment params

--- outsystems/vars/cicd_vars.py
+++ outsystems/vars/cicd_vars.py
@@ -2,9 +2,9 @@
 import os
 
 # Base CICD Probe Variables
-PROBE_HTTP_PROTO = os.environ['CICDProbeProto']
-PROBE_API_ENDPOINT = os.environ['CICDProbeAPIEndpoint']
-PROBE_API_VERSION = int(os.environ['CICDProbeAPIVersion'])
+PROBE_HTTP_PROTO = "https"
+PROBE_API_ENDPOINT = "CI_CDProbe/rest"
+PROBE_API_VERSION = 1
 
 # Scan Endpoints
 SCAN_BDD_TESTS_ENDPOINT = "ScanBDDTestEndpoints"
