6e1ff397e5f8ceaeda7b0d6da5bb68f09b68de43
João Furtado <joao.furtado@outsystems.com>
2019-03-11 11:07:09 +0000

    added basic setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+NAME = 'outsystems-pipeline'
+DESCRIPTION = 'Python pipeline to enable continuous testing using OutSystems.'
+AUTHOR = u'João Furtado, Miguel Afonso, Rui Mendes'
+EMAIL = u'joao.furtado@outsystems.com, miguel.afonso@outsystems.com, rui.mendes@outsystems.com'
+URL = 'https://github.com/OutSystems/outsystems-pipeline'
+KEYWORDS = [
+  '',
+]
+PACKAGES = [
+  'outsystems', 
+  'outsystems.bdd_framework', 
+  'outsystems.cicd_probe', 
+  'outsystems.exceptions', 
+  'outsystems.file_helpers', 
+  'outsystems.lifetime', 
+  'outsystems.pipeline', 
+  'outsystems.vars'
+]
+
+if __name__ == '__main__':  # Do not run setup() when we import this module.
+  setup(
+    name=NAME,
+    version='0.1',
+    description=DESCRIPTION,
+    keywords=' '.join(KEYWORDS),
+    author=AUTHOR,
+    author_email=EMAIL,
+    url=URL,
+    packages=PACKAGES,
+  )
