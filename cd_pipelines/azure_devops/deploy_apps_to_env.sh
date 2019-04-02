b14e486e62966e19196bc448523dc84d41e3ee60
João Furtado <joao.furtado@outsystems.com>
2019-04-02 11:19:20 +0100

    added bash script with parameters for app deployment

--- cd_pipelines/azure_devops/deploy_apps_to_env.sh
+++ cd_pipelines/azure_devops/deploy_apps_to_env.sh
@@ -1,10 +1,41 @@
 #!/bin/bash
 
-echo "Switch to Virtual Environment"
-source $ENVNAME/bin/activate
+if [[ $# -lt 18 ]] ; then
+  echo -e '\nNot enough parameters! You need to set the necessary parameters:\n'
+  echo -e '-e <python environment name> \t\t Python environment name, in order to use your pip dependencies.'
+  echo -e '-a <dir name> \t\t\t\t Artifacts directory used for cache files between pipeline scripts.'
+  echo -e '-u <url> \t\t\t\t URL for the environment containing the LifeTime. You dont need to set the API endpoint.'
+  echo -e '-t <token> \t\t\t\t API Token for the LifeTime service account.'
+  echo -e '-v <int> \t\t\t\t LifeTime API version number.'
+  echo -e '-s <int> \t\t\t\t Source environment name, where the apps you want to deploy are tagged.'
+  echo -e '-d <int> \t\t\t\t Destination environment name, where the apps you deploy will be.'
+  echo -e '-l <int> \t\t\t\t Comma separated list of applications you want to deploy.'
+  echo -e '-m <int> \t\t\t\t Message you want to set on the deployment plan in LifeTime.'
+  echo -e '\n\nusage: ./deploy_apps_to_env.sh -e <python environment name> -a <artifacts dir> -u <LT url> -t <LT token> -v <LT version> -s <source env name> -d <source env name> -l <app list> -m <deployment message>'
+  exit 1
+fi
 
-echo "Deploy apps to $DESTENV"
-python3 outsystems/pipeline/deploy_latest_tags_to_target_env.py --artifacts "$ARTIFACTSFOLDER" --lt_url $LTURL --lt_token $LTTOKEN --lt_api_version $LTAPIVERSION --source_env "$SOURCEENV" --destination_env "$DESTENV" --app_list "$APPLIST" --deploy_msg "$DEPLOYMSG"
+while getopts "e:a:u:t:v:s:d:l:m:" option 
+do
+  case "${option}"
+  in
+  e) env_name=${OPTARG};;
+  a) artifacts=${OPTARG};;
+  u) lt_url=${OPTARG};;
+  t) lt_token=${OPTARG};;
+  v) lt_api=${OPTARG};;
+  s) source_env=${OPTARG};;
+  d) dest_env=${OPTARG};;
+  l) app_list=${OPTARG};;
+  m) dep_msg=${OPTARG};;
+  esac
+done
+
+echo "Switch to Virtual Environment"
+source $env_name/bin/activate
+
+echo "Deploy apps to $dest_env"
+python3 outsystems/pipeline/deploy_latest_tags_to_target_env.py --artifacts "$artifacts" --lt_url $lt_url --lt_token $lt_token --lt_api_version $lt_api --source_env "$source_env" --destination_env "$dest_env" --app_list "$app_list" --deploy_msg "$dep_msg"
 
 echo "Leave the Virtual Environment for now"
 deactivate
