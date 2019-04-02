78c450fe88b262ad595558797786c89d1ff15393
João Furtado <joao.furtado@outsystems.com>
2019-04-02 10:42:07 +0100

    added bash script for fetch lt data

--- cd_pipelines/azure_devops/fetch_lt_data.sh
+++ cd_pipelines/azure_devops/fetch_lt_data.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
+if [[ $# -lt 10 ]] ; then
+  echo -e '\nNot enough parameters! You need to set the necessary parameters:\n'
+  echo -e '-e <python environment name> \t\t Python environment name, in order to use your pip dependencies.'
+  echo -e '-a <dir name> \t\t\t\t Artifacts directory used for cache files between pipeline scripts.'
+  echo -e '-u <url> \t\t\t\t URL for the environment containing the LifeTime. You dont need to set the API endpoint.'
+  echo -e '-t <token> \t\t\t\t API Token for the LifeTime service account.'
+  echo -e '-v <int> \t\t\t\t LifeTime API version number.'
+  exit 1
+fi
+
+while getopts "e:a:u:t:v:" option 
+do
+  case "${option}"
+  in
+  e) env_name=${OPTARG};;
+  a) artifacts=${OPTARG};;
+  u) lt_url=${OPTARG};;
+  t) lt_token=${OPTARG};;
+  v) lt_api=${OPTARG};;
+  esac
+done
+
 echo "Switch to Virtual Environment"
-source $ENVNAME/bin/activate
+source $env_name/bin/activate
 
 echo "Fetch LifeTime data"
-python3 outsystems/pipeline/fetch_lifetime_data.py --artifacts "$ARTIFACTSFOLDER" --lt_url $LTURL --lt_token $LTTOKEN --lt_api_version $LTAPIVERSION
+python3 outsystems/pipeline/fetch_lifetime_data.py --artifacts "$artifacts" --lt_url $lt_url --lt_token $lt_token --lt_api_version $lt_api
 
 echo "Leave the Virtual Environment for now"
 deactivate
