db2332a9d36eb52bb266c318ff3c3b6e6d79a44d
Miguel Afonso <41197994+miguel-afonso@users.noreply.github.com>
2019-05-31 15:34:16 +0100

    Moved python requirements from build-requirements.txt to setup.py

--- setup.py
+++ setup.py
@@ -19,14 +19,23 @@ PACKAGES = [
     'outsystems.vars'
 ]
 
+REQUIREMENTS = [
+  'python-dateutil==2.7.5',
+  'requests==2.20.1',
+  'unittest-xml-reporting==2.2.1',
+  'xunitparser==1.3.3',
+  'pytest==4.3.0'
+]
+
 if __name__ == '__main__':  # Do not run setup() when we import this module.
     setup(
         name=NAME,
-        version='0.1',
+        version='0.2',
         description=DESCRIPTION,
         keywords=' '.join(KEYWORDS),
         author=AUTHOR,
         author_email=EMAIL,
         url=URL,
         packages=PACKAGES,
+        install_requires=REQUIREMENTS
     )
