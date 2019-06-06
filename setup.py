47871b19981f2dc8c123cbe2420e533b4be49947
João Furtado <joao.furtado@outsystems.com>
2019-06-06 14:32:55 +0100

    version file and setup that receives toggles to upgrade the version

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import argparse, sys
 from distutils.core import setup
 
 NAME = 'outsystems-pipeline'
@@ -24,9 +25,47 @@ PACKAGES = [
 ]
 
 if __name__ == '__main__':  # Do not run setup() when we import this module.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--revision", help="Toggle if you're doing a revision version.", action="store_true")
+    parser.add_argument("--minor", help="Toggle if you're doing a minor version.", action="store_true")
+    parser.add_argument("sdist")
+    args = parser.parse_args()
+
+    with open("VERSION", 'r') as version_file:
+        version = version_file.read().replace('\n','')
+        version_array = version.split('.')
+
+    if args.revision:
+        if len(version_array) > 2: # Increments the previous beta version
+            revision_version = int(version_array[-1])
+            version_array[-1] = str(revision_version + 1)
+        else: # no beta version, creates one with 1
+            version_array.extend("1")
+    elif args.minor:
+        if len(version_array) > 2: # Removes the previous beta version
+            version_array[-1] = "0" # remove revision
+        else: # forces 3 part release versions
+            while len(version_array) < 3:
+                version_array.extend("0")
+        minor_version = int(version_array[-2])
+        version_array[-2] = str(minor_version + 1)
+    else:
+        major_version = int(version_array[0])
+        version_array = [str(major_version + 1), "0", "0"]
+
+    version = ".".join(version_array)
+
+    if "--revision" in sys.argv:
+        sys.argv.remove("--revision")
+    if "--minor" in sys.argv:
+        sys.argv.remove("--minor")
+    
+    with open("VERSION", 'w') as version_file:
+        version_file.write(version)
+
     setup(
         name=NAME,
-        version='0.1.5',
+        version=version,
         description=DESCRIPTION,
         keywords=' '.join(KEYWORDS),
         author=AUTHOR,
