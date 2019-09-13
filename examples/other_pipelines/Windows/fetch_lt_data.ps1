03d4cda5a260e8c6c3387764ffa3533c9072abda
João Furtado <joao.furtado@outsystems.com>
2019-09-13 17:43:23 +0100

    adding stashing to the PS scripts but leaving it commented out. It's only meant to be used in Azure DevOps

--- examples/other_pipelines/Windows/fetch_lt_data.ps1
+++ examples/other_pipelines/Windows/fetch_lt_data.ps1
@@ -1,14 +1,14 @@
 param(
-  [Parameter(Mandatory=$true)]
-  [string]$PythonEnv,
-  [Parameter(Mandatory=$true)]
-  [string]$ArtifactDir,
-  [Parameter(Mandatory=$true)]
-  [string]$LifeTimeUrl,
-  [Parameter(Mandatory=$true)]
-  [string]$LifeTimeToken,
-  [Parameter(Mandatory=$true)]
-  [int]$LifeTimeApi
+    [Parameter(Mandatory = $true)]
+    [string]$PythonEnv,
+    [Parameter(Mandatory = $true)]
+    [string]$ArtifactDir,
+    [Parameter(Mandatory = $true)]
+    [string]$LifeTimeUrl,
+    [Parameter(Mandatory = $true)]
+    [string]$LifeTimeToken,
+    [Parameter(Mandatory = $true)]
+    [int]$LifeTimeApi
 )
 
 Write-Host "Switch to Virtual Environment"
@@ -17,5 +17,18 @@ Write-Host "Switch to Virtual Environment"
 Write-Host "Fetch LifeTime data"
 python -m outsystems.pipeline.fetch_lifetime_data --artifacts "$ArtifactDir" --lt_url $LifeTimeUrl --lt_token $LifeTimeToken --lt_api_version $LifeTimeApi
 
+# Store the exit status from the command above, to make it the exit status of this script
+$status_code = $LASTEXITCODE
+
 Write-Host "Leave the Virtual Environment for now"
 deactivate
+
+#### For Azure DevOps, uncomment the next lines ####
+#Write-Host "Stashing the *.cache generated in the pipeline logs"
+#$cache_files = Get-ChildItem -Path "$PWD\$(ArtifactsFolder)\*.cache"
+#foreach ($cfile in $cache_files) {
+#  Write-Host "Stashing $cfile"
+#  Write-Output "##vso[task.uploadfile]$cfile"
+#}
+
+exit $status_code
--- examples/other_pipelines/Windows/running_test_endpoints.ps1
+++ examples/other_pipelines/Windows/running_test_endpoints.ps1
@@ -1,8 +1,8 @@
 param(
-  [Parameter(Mandatory=$true)]
-  [string]$PythonEnv,
-  [Parameter(Mandatory=$true)]
-  [string]$ArtifactDir
+    [Parameter(Mandatory = $true)]
+    [string]$PythonEnv,
+    [Parameter(Mandatory = $true)]
+    [string]$ArtifactDir
 )
 
 
@@ -12,5 +12,10 @@ Write-Host "Switch to Virtual Environment"
 Write-Host "Building the test endpoints"
 python -m outsystems.pipeline.evaluate_test_results --artifacts "$ArtifactDir"
 
+# Store the exit status from the command above, to make it the exit status of this script
+$status_code = $LASTEXITCODE
+
 Write-Host "Leave the Virtual Environment for now"
 deactivate
+
+exit $status_code
