556a5f0b21e4eeca3fdcc167437bff31e4ca215d
Paulo Ribeiro <paulo.ribeiro@outsystems.com>
2019-10-01 16:06:47 +0100

    Fix typo in logging for deploy_latest_tags_to_target_env.py

--- outsystems/pipeline/deploy_latest_tags_to_target_env.py
+++ outsystems/pipeline/deploy_latest_tags_to_target_env.py
@@ -81,12 +81,12 @@ def main(artifact_dir: str, lt_http_proto: str, lt_url: str, lt_api_endpoint: st
             if app_status["AppStatusInEnvs"][0]["BaseApplicationVersionKey"] != app["VersionKey"]:
                 # If it's not, save the key of the tagged app, to deploy later
                 to_deploy_app_keys.append(app["VersionKey"])
-                print("App {} with version {} does not exist in {} environment. Ignoring check and deploy it.".format(app["Name"], app["Version"], dest_env))
+                print("App {} with version {} does not exist in {} environment. Ignoring check and deploying it.".format(app["Name"], app["Version"], dest_env))
             else:
                 print("Skipping app {} with version {}, since it's already deployed in {} environment.".format(app["Name"], app["Version"], dest_env))
         except AppDoesNotExistError:
             to_deploy_app_keys.append(app["VersionKey"])
-            print("App {} with version {} does not exist in {} environment. Ignoring check and deploy it.".format(app["Name"], app["Version"], dest_env))
+            print("App {} with version {} does not exist in {} environment. Ignoring check and deploying it.".format(app["Name"], app["Version"], dest_env))
 
     # Check if there are apps to be deployed
     if len(to_deploy_app_keys) == 0:
