00a57c265e15e53b3d6126fc026fa53bde8fc6af
Duarte Castaño <duarte.castano@outsystems.com>
2021-09-21 13:51:46 +0100

    evaluate if input param matches any global var

--- outsystems/configurations/pipeline_configurations.py
+++ outsystems/configurations/pipeline_configurations.py
@@ -5,22 +5,17 @@ from configparser import ConfigParser
 # Custom Modules
 # Variables
 from outsystems.vars.pipeline_vars import SSL_CERT_VERIFY, DEPLOYMENT_TIMEOUT_IN_SECS
-# Exceptions
-from outsystems.exceptions.configuration_not_found import ConfigurationNotFoundError
 
 
 # Returns the configuration value which may be defined in a properties file.
 def get_conf_value(conf: str):
-
-    # verify if given conf is settable via conf_file
-    if conf not in ("SSL_CERT_VERIFY", "DEPLOYMENT_TIMEOUT_IN_SECS"):
-        raise ConfigurationNotFoundError
 
     _create_global_conf()
     global global_config
 
     # if config value already has been define in global dict then return it
     if conf in global_config:
+        print("return default value: {} -> {}".format(conf, global_config[conf]), flush=True)
         return global_config[conf]
 
     # if not, try to retrieve it from the config file
@@ -30,14 +25,13 @@ def get_conf_value(conf: str):
         if config_file.has_section('PIPELINE_CONFIG'):
             details_dict = dict(config_file.items('PIPELINE_CONFIG'))
             if conf.lower() in details_dict:
+                print("set value from config file: {} -> {}".format(conf, global_config[conf]), flush=True)
                 global_config[conf] = details_dict[conf.lower()]
 
     # If configuration is not yet defined at this moment then set it to its default value
     if conf not in global_config:
-        if conf == "SSL_CERT_VERIFY":
-            global_config[conf] = SSL_CERT_VERIFY
-        elif conf == "DEPLOYMENT_TIMEOUT_IN_SECS":
-            global_config[conf] = DEPLOYMENT_TIMEOUT_IN_SECS
+        print("return default value: {} -> {}".format(conf, global_config[conf]), flush=True)
+        global_config[conf] = eval(conf)
 
     return global_config[conf]
 
--- outsystems/exceptions/configuration_not_found.py
+++
@@ -1,2 +0,0 @@
-class ConfigurationNotFoundError(Exception):
-    pass
