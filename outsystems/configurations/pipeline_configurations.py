fb2e898ead6cb6dcf6d347d164cda36d711f9fd0
Duarte Castaño <duarte.castano@outsystems.com>
2021-09-23 14:48:36 +0100

    evaluate config value directly from eval function

--- outsystems/configurations/pipeline_configurations.py
+++ outsystems/configurations/pipeline_configurations.py
@@ -2,10 +2,6 @@
 import os
 from configparser import ConfigParser
 
-# Custom Modules
-# Variables
-from outsystems.vars.pipeline_vars import SSL_CERT_VERIFY, DEPLOYMENT_TIMEOUT_IN_SECS
-
 
 # Returns the configuration value which may be defined in a properties file.
 def get_conf_value(conf: str):
@@ -18,7 +14,7 @@ def get_conf_value(conf: str):
         return global_config[conf]
 
     # Check if config file path is defined in the env var
-    # Retrieve the config value from the config file
+    # Retrieve the config value from the properties file
     elif "OS_PIPELINE_CONFIG_FILE" in os.environ:
         config_file = ConfigParser()
         config_file.read(os.environ["OS_PIPELINE_CONFIG_FILE"])
@@ -28,9 +24,9 @@ def get_conf_value(conf: str):
                 global_config[conf] = details_dict[conf.lower()]
                 print("[PIPELINE_CONFIG] Configuration overridden: '{}' has now the value '{}'".format(conf, global_config[conf]), flush=True)
 
-    # If configuration is not yet defined at this moment then set it to its default value
+    # If global configuration is not yet defined at this moment then set it to its default value
     if conf not in global_config:
-        global_config[conf] = eval(conf)
+        global_config[conf] = eval("outsystems.vars.pipeline_vars.{}".format(conf))
 
     return global_config[conf]
 
