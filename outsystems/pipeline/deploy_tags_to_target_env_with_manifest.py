f68e4949271a2d3058eeb0e9849bb9e5fcca177b
Rui Mendes <rui.mendes@outsystems.com>
2022-01-03 19:09:02 +0000

    Removed apply_configuration_values_to_target_env function since it was refactored to a separate file.

--- outsystems/pipeline/deploy_tags_to_target_env_with_manifest.py
+++ outsystems/pipeline/deploy_tags_to_target_env_with_manifest.py
@@ -18,9 +18,7 @@ else:  # Else just add the project dir
 # Variables
 from outsystems.vars.file_vars import ARTIFACT_FOLDER
 from outsystems.vars.lifetime_vars import LIFETIME_HTTP_PROTO, LIFETIME_API_ENDPOINT, LIFETIME_API_VERSION
-from outsystems.vars.manifest_vars import MANIFEST_APPLICATION_VERSIONS, MANIFEST_FLAG_IS_TEST_APPLICATION, MANIFEST_CONFIG_ITEM_KEY, \
-    MANIFEST_CONFIG_ITEM_NAME, MANIFEST_CONFIG_ITEM_TARGET_VALUE, MANIFEST_CONFIG_ITEM_TYPE, MANIFEST_MODULE_KEY
-from outsystems.vars.properties_vars import PROPERTY_TYPE_SITE_PROPERTY
+from outsystems.vars.manifest_vars import MANIFEST_APPLICATION_VERSIONS, MANIFEST_FLAG_IS_TEST_APPLICATION
 from outsystems.vars.pipeline_vars import QUEUE_TIMEOUT_IN_SECS, SLEEP_PERIOD_IN_SECS, CONFLICTS_FILE, \
     REDEPLOY_OUTDATED_APPS, DEPLOYMENT_TIMEOUT_IN_SECS, DEPLOYMENT_RUNNING_STATUS, DEPLOYMENT_WAITING_STATUS, \
     DEPLOYMENT_ERROR_STATUS_LIST, DEPLOY_ERROR_FILE, ALLOW_CONTINUE_WITH_ERRORS
@@ -31,8 +29,7 @@ from outsystems.lifetime.lifetime_deployments import get_deployment_status, get_
     send_deployment, delete_deployment, start_deployment, continue_deployment, get_running_deployment
 from outsystems.file_helpers.file import store_data
 from outsystems.lifetime.lifetime_base import build_lt_endpoint
-from outsystems.manifest.manifest_base import get_environment_details, get_deployment_notes, get_configuration_items_for_environment
-from outsystems.properties.properties_set_value import set_site_property_value
+from outsystems.manifest.manifest_base import get_environment_details, get_deployment_notes
 # Exceptions
 from outsystems.exceptions.app_does_not_exist import AppDoesNotExistError
 
@@ -100,34 +97,6 @@ def check_if_can_deploy(artifact_dir: str, lt_endpoint: str, lt_api_version: str
             app_keys.append(generate_deploy_app_key(lt_api_version, app["VersionKey"]))
             print("App {} with version {} does not exist in {} environment. Ignoring check and deploy it.".format(app["Name"], app["Version"], env_name), flush=True)
     return app_keys
-
-
-# Function to apply configuration values to a target environment
-def apply_configuration_values_to_target_env(artifact_dir: str, lt_url: str, lt_token: str, target_env_label: str, trigger_manifest: dict):
-
-    # Tuple with (EnvName, EnvKey): target_env_tuple[0] = EnvName; target_env_tuple[1] = EnvKey
-    target_env_tuple = get_environment_details(trigger_manifest, target_env_label)
-
-    # Get configuration items defined in the manifest for target environment
-    config_items = get_configuration_items_for_environment(trigger_manifest, target_env_tuple[1])
-
-    # Check if there are any configuration item values to apply for target environment
-    if config_items:
-        print("Applying new values to configuration items in {} (Label: {})...".format(target_env_tuple[0], target_env_label), flush=True)
-    else:
-        print("No configuration item values were found in the manifest for {} (Label: {}).".format(target_env_tuple[0], target_env_label), flush=True)
-
-    # Apply target value for each configuration item according to its type
-    for cfg_item in config_items:
-        if cfg_item[MANIFEST_CONFIG_ITEM_TYPE] == PROPERTY_TYPE_SITE_PROPERTY:
-            result = set_site_property_value(
-                lt_url, lt_token, cfg_item[MANIFEST_MODULE_KEY], target_env_tuple[1], cfg_item[MANIFEST_CONFIG_ITEM_KEY], cfg_item[MANIFEST_CONFIG_ITEM_TARGET_VALUE])
-            if result["Success"]:
-                print("New value successfully applied to configuration item '{}' ({}).".format(cfg_item[MANIFEST_CONFIG_ITEM_NAME], cfg_item[MANIFEST_CONFIG_ITEM_TYPE]), flush=True)
-            else:
-                print("Unable to apply new value to configuration item '{}' ({}).\nReason: {}".format(cfg_item[MANIFEST_CONFIG_ITEM_NAME], cfg_item[MANIFEST_CONFIG_ITEM_TYPE], result["Message"]), flush=True)
-        else:
-            raise NotImplementedError("Configuration item type '{}' not supported.".format(cfg_item[MANIFEST_CONFIG_ITEM_TYPE]))
 
 
 def main(artifact_dir: str, lt_http_proto: str, lt_url: str, lt_api_endpoint: str, lt_api_version: int, lt_token: str, source_env_label: str, dest_env_label: str, include_test_apps: bool, trigger_manifest: dict):
