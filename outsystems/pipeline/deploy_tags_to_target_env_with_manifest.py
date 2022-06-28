48369d290eadd5500302f59d0c7f1c0ef26e68ea
Rui Mendes <rui.mendes@outsystems.com>
2022-06-28 16:20:51 +0100

    Removed stale manifest validation when using trigger manifest artifact.

--- outsystems/pipeline/deploy_tags_to_target_env_with_manifest.py
+++ outsystems/pipeline/deploy_tags_to_target_env_with_manifest.py
@@ -83,11 +83,8 @@ def check_if_can_deploy(artifact_dir: str, lt_endpoint: str, lt_api_version: str
                     if app_in_env["BaseApplicationVersionKey"] != app["VersionKey"]:
                         # The version is not the one deployed -> need to compare the version tag
                         app_in_env_data = get_application_version(artifact_dir, lt_endpoint, lt_token, False, app_in_env["BaseApplicationVersionKey"], app_key=app["Key"])
-                        # If the version in the environment is higher than the one in the manifest -> stale pipeline -> abort
-                        if parse_version(app_in_env_data["Version"]) > parse_version(app["Version"]):
-                            print("The deployment manifest is stale. The Application {} needs to be deployed with version {} but then environment {} has the version {}.\nReason: VersionTag is inferior to the VersionTag already deployed.\nAborting the pipeline.".format(app["Name"], app["Version"], env_name, app_in_env_data["Version"]), flush=True)
-                            sys.exit(1)
-                        elif parse_version(app_in_env_data["Version"]) == parse_version(app["Version"]):
+                        # If the version in the target environment has the same version number -> skip deployment
+                        if parse_version(app_in_env_data["Version"]) == parse_version(app["Version"]):
                             print("Skipping application {} with version {}, since it's already deployed in {} environment.\nReason: VersionTag is equal.".format(app["Name"], app["Version"], env_name), flush=True)
                         else:
                             # Generated app_keys for deployment plan based on the running version
